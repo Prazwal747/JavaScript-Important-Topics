83eb8ee6f0ca814002d2b698cc1f605eda9acdc8
Prazwal747 <prajwalshivapurmath@gmail.com>
2024-02-09 16:21:34 +0530

    array methods simple one practise

---
+++ Array.js
@@ -0,0 +1,25 @@
+let arr1 = ["C","c++","python"];
+let arr2 = ["java","js","rust"];
+
+let result = arr1.concat(arr2);
+console.log(result);
+
+// ----------------------------------------------------------
+
+let a = ["Angular" ,"Node.js" ,"React","Jquery"];
+
+var res = a.copyWithin(1,2);
+console.log(res);
+
+let res2 = a.copyWithin(0,1,2);
+console.log(res2);
+
+let res3 = a.copyWithin(0,1,3);
+console.log(res3);
+
+
+//provinf only two params
+
+let demoarr = [1,2,3,4,5,6];
+let der = demoarr.copyWithin(0,2);
+console.log(der);
